256e5a3dfe8bd1eaadccfcc085e7168b0739e2b8
Avonts <vinialves022@gmail.com>
2022-01-11 17:50:22 -0300

    Add Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,5 @@
+export function Form() {
+    return (
+        <h1>Página de formulário</h1>
+    )
+}
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,5 @@
+export function Listing() {
+    return (
+        <h1>Página de listagem</h1>
+    );
+}
